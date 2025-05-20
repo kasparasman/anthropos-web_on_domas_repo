2f4c0a5927114ffc4ea61b5f9e910198815fba92
kasparasman <maniusiskasparas@gmail.com>
2025-05-20 10:22:28 +0300

    index page changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,15 +23,19 @@ interface HomeProps {
 /* ------------------------------------------------------------------ */
 export default function Home({ dexData, topics, assets  }: HomeProps) {
 
-    return (
-    <main className="flex flex-col items-center bg-background p-6 md:p-24 space-y-12">
+  return (
+  <main className="flex flex-col items-center bg-background p-4 md:p-6">
+    <div className="max-w-5xl space-y-10">
       <Banner />
-      <StatsSection data={dexData} />
-      <RightPanel topics={topics} />
+      <div className="flex flex-col md:flex-row gap-6 w-full">
+        <StatsSection data={dexData} />
+        <RightPanel topics={topics} />
+      </div>
       <AssetCarousel assets={assets} loop autoplay speed={4000} />
+    </div>
 
-    </main>
-  )
+  </main>
+)
 }
 
 /* ------------------------------------------------------------------ */
