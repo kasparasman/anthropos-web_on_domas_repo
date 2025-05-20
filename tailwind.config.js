c23d737183fa308a9ecf6728c065e8ae11d49431
kasparasman <maniusiskasparas@gmail.com>
2025-05-20 10:42:58 +0300

    merged manually

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,22 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './src/**/*.{js,ts,jsx,tsx}',
+    './styles/**/*.{css}'
   ],
   theme: {
     extend: {
       colors: {
-        background: 'var(--background)',   // your light/dark CSS vars
+        background: 'var(--background)',
         foreground: 'var(--foreground)',
         main:       'var(--Main)',
+        main:       'var(--main)',
+        smoke:      'var(--smoke)',
+        dim_smoke:  'var(--dim_smoke)',
+        gray:       'var(--gray)',
       },
       fontFamily: {
         sans:  ['var(--font-geist-sans)', 'Montserrat', ...defaultTheme.fontFamily.sans],
-        mono:  ['var(--font-geist-mono)', 'Monument Extended', ...defaultTheme.fontFamily.mono],
+        monu:  ['var(--font-geist-mono)', 'Monument Extended', ...defaultTheme.fontFamily.mono],
         inter: ['Inter', ...defaultTheme.fontFamily.sans],
       },
       spacing: {
