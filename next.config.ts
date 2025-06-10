25e3c333c4a75010f0f8cbe67d14168811cb7d59
kasparasman <maniusiskasparas@gmail.com>
2025-06-10 16:26:33 +0300

    webpack

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) config.plugins.push(new NodePolyfillPlugin());
+  webpack: (config) => {
+    if (!config.isServer) config.resolve.fallback = { fs: false };
     return config;
   },
 
